3ff03bb8588d80e630146d47dbfcb0f2a688a4e8
sunhwa <2017sunhwa@gmail.com>
2021-08-03 16:31:55 +0900

    strapi deploy test

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c124ef111dc623f9ededeaa059e19a4f'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e8976c3d-7dc4-45c4-81a3-e59da06da1c7'
+};
